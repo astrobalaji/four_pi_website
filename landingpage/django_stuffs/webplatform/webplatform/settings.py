d9226078f1ff13bf8cb24a7a3676cbdf71176cbf
Balaji Muthusubramanian <astrobalaji@Balajis-Air.fritz.box>
2020-01-30 11:02:28 +0100

    migrated DB to mysql

--- landingpage/django_stuffs/webplatform/webplatform/settings.py
+++ landingpage/django_stuffs/webplatform/webplatform/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'webplatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'astronom441453_bruno',
+        'USER': 'astro_centaur',
+        'PASSWORD': 'Ad_Astra_42',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
