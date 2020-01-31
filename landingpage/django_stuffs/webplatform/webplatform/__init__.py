adff69f3a41450ff9a7f757c5950a46d85f179f8
Balaji Muthusubramanian <astrobalaji@Balajis-Air.fritz.box>
2020-01-31 13:06:34 +0100

    pymysql try - whyy??

--- landingpage/django_stuffs/webplatform/webplatform/__init__.py
+++ landingpage/django_stuffs/webplatform/webplatform/__init__.py
@@ -1,3 +1,11 @@
-import pymysql
-#pymysql.version_info = (1, 3, 13, "final", 0)
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
+
+'''import pymysql
+pymysql.version_info = (1, 3, 13, "final", 0)
 pymysql.install_as_MySQLdb()
+'''
