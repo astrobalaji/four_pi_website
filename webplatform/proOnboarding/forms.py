52055b231e564e45fd6099f37138f7113c1be625
Balaji Muthusubramanian <astrobalaji@Balajis-Air.fritz.box>
2020-02-10 09:26:14 +0100

    added professsional onboarding

---
+++ webplatform/proOnboarding/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from .models import proOB
+field_choices = [('yso','Young Stellar Objects'), ('planets', 'Planets'), ('exop', 'Exoplanets'), ('agn', 'Active Galactic Nucleii'), ('galactic', 'Galaxies'), ('extragal', 'Extra Galactic'), ('cosmology', 'Cosmology')]
+class proOnboard(forms.ModelForm):
+    affiliation = forms.CharField(label = 'Enter your affilliated university', required = True)
+    field_of_interest = forms.ChoiceField(choices = field_choices, required = True)
+
+    class Meta:
+        model = proOB
+        exclude = ['user_id', 'fov']
--- webplatform/webplatform/urls.py
+++ webplatform/webplatform/urls.py
@@ -5,6 +5,7 @@ from coming_soon import views as cs_views
 from signup.views import SignUpView, ActivateAccount
 from confirm_reg import views as conf_email_views
 from amateurOnboarding.views import AmaOBViews
+from proOnboarding.views import proOBViews
 from user_home import views as home_view
 
 urlpatterns = [
@@ -17,5 +18,6 @@ urlpatterns = [
     path('signup/activate/confirm_email', conf_email_views.index),
     path('activate/dir_sel/', conf_email_views.init_sel),
     path('onboarding/amateur/', AmaOBViews.as_view()),
-    path('user/home/', home_view.index)
+    path('user/home/', home_view.index),
+    path('onboarding/professional/', proOBViews.as_view()),
 ]
