a59a14e70460aeced2c14149203f43091d50f1f6
Balaji Muthusubramanian <astrobalaji@Balajis-Air.fritz.box>
2020-03-04 13:28:32 +0100

    updated fields on the amateur onboarding form

--- webplatform/amateurOnboarding/forms.py
+++ webplatform/amateurOnboarding/forms.py
@@ -10,10 +10,12 @@ class AmaOnboarding(forms.ModelForm):
     lon = forms.FloatField(label = 'Latitude (decimals)', required = True, min_value = -180., max_value = 180.)
     tz = forms.FloatField(label = 'Time zone +/- hours in decimals from UTC', required = True, min_value = -12., max_value = 12.)
     obs_img = forms.ImageField(label = 'Upload a picture of your setup (preferrably a jpg)', required = True)
+    telescope_mod = forms.CharField(label=' Manufacturer / Model of the telescope', required = True)
     telescope_aper = forms.FloatField(label = 'Telescope Aperture (mm)', required = True, min_value = 0.)
     telescope_flength = forms.FloatField(label = 'Telescope Focal Length (mm)', required = True, min_value = 0.)
     det_mod = forms.CharField(label = 'Model name of the detector/Camera', required = True)
-    det_pix_scale = forms.FloatField(label = 'pixel scale of the detector (microns)', min_value = 0., required = True)
+    chip_name = forms.CharField(label = 'Manufacturer / Model of the detector chip', required = False, help_text='optional but highly desired')
+    det_pix_scale = forms.FloatField(label = 'Physical pixel size of the detector (microns)', min_value = 0., required = True, help_text = 'average if pixel is not a square')
     detector_dimensions = forms.CharField(label = 'dimensions of the detector pixels (# of pixels)', help_text = 'required format: 0000x0000 pixels', required = True)
     read_noise = forms.FloatField(label = 'Read Noise of the Detector (in electrons)', required = True)
     class Meta:
