0162d28855f52c85df1cf27cabb4e06410b2ef6f
Balaji Muthusubramanian <astrobalaji@Balajis-Air.fritz.box>
2020-03-12 21:46:57 +0100

    fixed obs calc widget

--- webplatform/observability_calc/forms.py
+++ webplatform/observability_calc/forms.py
@@ -5,4 +5,4 @@ from django.utils.translation import ugettext_lazy as _
 class ObsSettings(forms.Form):
 
     exposure_time = forms.FloatField(label = 'Enter your Desired Exposure time in seconds')
-    further_instructions = forms.CharField(label = 'Enter further instructions for the amateur astronomer', widget = forms.SelectDateWidget)
+    further_instructions = forms.CharField(label = 'Enter further instructions for the amateur astronomer', widget = forms.Textarea)
