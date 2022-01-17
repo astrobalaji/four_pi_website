05f72241aa044aaee753980981ea1e6ec61d62c2
Balaji Muthusubramanian <astrobalaji@Balajis-MacBook-Air.local>
2022-01-18 00:30:10 +0530

    modified landingpage

--- webplatform/app/coming_soon/views.py
+++ webplatform/app/coming_soon/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-        return render(request, 'coming_soon.html')
+        return render(request, 'landingpage.html')#'coming_soon.html')
